aefe1b206aeb61f8e5812e7a1b0c7d1466cbb951
Nate Koike <nkoike1998@gmail.com>
2020-09-05 08:41:34 +0900

    fixed indexing issue

--- substitution.cc
+++ substitution.cc
@@ -5,7 +5,7 @@ using namespace std;
 // a struct to hold the substitutions
 struct subs{
   string old = "";
-  string replace = "";
+  string replacement = "";
 };
 
 // a list node
@@ -23,35 +23,35 @@ struct slist{
   // remove the first node in the list
   void pop(){
     // save the next head value
-    node * tmp = this -> head -> next;
+    node * tmp = this->head->next;
 
     // delete the substitution in the head
-    delete this -> head -> sub;
+    delete this->head->sub;
 
     // delete the struct at the head
-    delete this -> head;
+    delete this->head;
 
     // move the head pointer to the first node in the list
-    this -> head = tmp;
+    this->head = tmp;
   }
 
   // add a new node to the front of the list
   void add(subs * sub){
     // make a new node
-    node * new_head =new node;
+    node * new_head = new node;
 
     // set the necessary values
-    new_head -> next = this -> head;
-    new_head -> sub = sub;
+    new_head->next = this->head;
+    new_head->sub = sub;
 
     // set the head of the list to be the
-    this -> head = new_head;
+    this->head = new_head;
   }
 };
 
 // delete the singly linked list and all the substitution structs within it
 void delete_list(slist * & list){
-  for (; list; list->pop());
+  for (; list->head; list->pop());
 }
 
 // =============================================================================
@@ -84,9 +84,46 @@ string split(string src, const char & tok){
   return sp;
 }
 
+// make a subs struct from the provided string
+subs * make_sub(string sub){
+  // make the struct
+  subs * new_sub = new subs;
+
+  // save the old sub
+  string old = split(sub, '/');
+
+  // assign the old and replacement strings
+  new_sub->old = old;
+  new_sub->replacement = sub.substr(old.length() + 1);
+
+  return new_sub;
+}
+
 // separate the substitutions into structs and return the list of substitutions
 slist * get_subs(string substitutions){
-  return NULL;
+  // make a list
+  slist * list = new slist;
+
+  // get the first substitution to make
+  string sub = split(substitutions, ':');
+
+  // run until there is only one substitution pair left to make
+  while (sub != substitutions){
+    // make and add the substitution to the list
+    list->add(make_sub(sub));
+
+    // remove the old substitution
+    substitutions = substitutions.substr(sub.length() + 1);
+
+    // get a new substitution
+    sub = split(substitutions, ':');
+  }
+
+  // add the last substitution
+  list->add(make_sub(sub));
+
+  // return the list
+  return list;
 }
 
 // get an input that includes newlines
@@ -111,5 +148,13 @@ int main(){
 
   get_input(src);
 
-  cout << split(substitutions, ':') << endl;
+  // get the replacements
+  slist * replacements = get_subs(substitutions);
+
+  // delete the replacements
+  delete_list(replacements);
+
+  // print the replacements
+  for (node * f = replacements->head; f; f = f->next)
+    cout << f->sub->old << " " << f->sub->replacement << endl;
 }

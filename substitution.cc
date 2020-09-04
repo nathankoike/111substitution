a2c9f8efc495f2d6f04127777212c2456f301682
Nate Koike <nkoike1998@gmail.com>
2020-09-05 07:32:00 +0900

    fixed syntax errors

--- substitution.cc
+++ substitution.cc
@@ -1,9 +1,12 @@
+#include <cstdlib>
+#include <iostream>
 using namespace std;
+
 
 // a struct to hold the substitutions
 struct subs{
   string old;
-  string new;
+  string replace;
 };
 
 // a list node
@@ -15,11 +18,25 @@ struct node{
 struct slist{
   node* head;
   node* tail;
+
+  void pop(){
+    // save the next head value
+    node * tmp = this -> head -> next;
+
+    // delete the substitution in the head
+    delete this -> head -> sub;
+
+    // delete the struct at the head
+    delete this -> head;
+
+    // move the head pointer to the first node in the list
+    this -> head = tmp;
+  }
 };
 
 // delete the singly linked list and all the substitution structs within it
 void delete_list(slist * list){
-  for (; list; list = )
+  for (; list; list = NULL);
 }
 
 // apply all changes to the source text

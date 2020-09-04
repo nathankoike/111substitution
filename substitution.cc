281c2f3c335df25f41bb858c2c3f367b0b9a33c3
Nate Koike <nkoike1998@gmail.com>
2020-09-05 07:27:59 +0900

    started implementing structs for project

--- substitution.cc
+++ substitution.cc
@@ -1,1 +1,28 @@
+using namespace std;
 
+// a struct to hold the substitutions
+struct subs{
+  string old;
+  string new;
+};
+
+// a list node
+struct node{
+  subs * sub;
+  node * next;
+};
+
+struct slist{
+  node* head;
+  node* tail;
+};
+
+// delete the singly linked list and all the substitution structs within it
+void delete_list(slist * list){
+  for (; list; list = )
+}
+
+// apply all changes to the source text
+void apply(){}
+
+int main(){}

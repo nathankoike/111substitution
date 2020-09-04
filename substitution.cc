2bd5ea64a114ae438d6a3a5802509c594616211a
Nate Koike <nkoike1998@gmail.com>
2020-09-05 07:38:02 +0900

    wrote more member functions for list struct

--- substitution.cc
+++ substitution.cc
@@ -5,20 +5,22 @@ using namespace std;
 
 // a struct to hold the substitutions
 struct subs{
-  string old;
-  string replace;
+  string old = "";
+  string replace = "";
 };
 
 // a list node
 struct node{
-  subs * sub;
-  node * next;
+  subs * sub  = NULL;
+  node * next = NULL;
 };
 
+// implement a list
 struct slist{
-  node* head;
-  node* tail;
+  node* head = NULL;
+  node* tail = NULL;
 
+  // remove the first node in the list
   void pop(){
     // save the next head value
     node * tmp = this -> head -> next;
@@ -32,11 +34,24 @@ struct slist{
     // move the head pointer to the first node in the list
     this -> head = tmp;
   }
+
+  // add a new node to the front of the list
+  void add(subs * sub){
+    // make a new node
+    node * new_head =new node;
+
+    // set the necessary values
+    new_head -> next = this -> head;
+    new_head -> sub = sub;
+
+    // set the head of the list to be the
+    this -> head = new_head;
+  }
 };
 
 // delete the singly linked list and all the substitution structs within it
 void delete_list(slist * list){
-  for (; list; list = NULL);
+  for (; list; list->pop());
 }
 
 // apply all changes to the source text
